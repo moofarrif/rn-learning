bf77e0a8ee847529a94f201edddb2908fd2b6b62
Jhon Moofarrif <jhon.moofarrif@gmail.com>
2025-04-08 23:56:43 -0500

    feat: init pokedex

--- 07-compApp/src/presentation/context/ThemeContext.tsx
+++ 07-compApp/src/presentation/context/ThemeContext.tsx
@@ -20,31 +20,31 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: PropsWithChildren) => {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
   const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light');
 
   const isDark = currentTheme === 'dark';
   const colors = isDark ? darkColors : lightColors;
 
-  useEffect(() => {
-    if (colorScheme === 'dark') {
-      setCurrentTheme('dark');
-    } else {
-      setCurrentTheme('light');
-    }
-  }, [colorScheme]);
-
   // useEffect(() => {
-  //   const subscription = AppState.addEventListener('change', nextAppState => {
-  //     const colorScheme = Appearance.getColorScheme();
-  //     setCurrentTheme( colorScheme === 'dark' ? 'dark' : 'light')
+  //   if (colorScheme === 'dark') {
+  //     setCurrentTheme('dark');
+  //   } else {
+  //     setCurrentTheme('light');
+  //   }
+  // }, [colorScheme]);
 
-  //   });
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      const colorScheme = Appearance.getColorScheme();
+      setCurrentTheme( colorScheme === 'dark' ? 'dark' : 'light')
 
-  //   return () => {
-  //     subscription.remove();
-  //   };
-  // }, []);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   const setTheme = (theme: ThemeColor) => {
     setCurrentTheme(theme);
